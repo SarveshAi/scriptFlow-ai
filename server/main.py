8b62d732096d9651f1be945140e0f9e4572a0320
SarveshAi <sarveshrj22@gmail.com>
2026-02-26 01:50:53 +0530

    switched to gemini flash from Ollama

--- server/main.py
+++ server/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from langchain_ollama import OllamaLLM, OllamaEmbeddings
+from dotenv import load_dotenv
+# from langchain_ollama import OllamaLLM, OllamaEmbeddings
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_chroma import Chroma
 from langchain_community.vectorstores import Chroma
 import os
 
+load_dotenv()
 app = FastAPI()
 
 # app.add_middleware(
@@ -11,11 +15,20 @@ app = FastAPI()
 #     allow_methods=["*"],
 #     allow_headers=["*"],
 # )
-ollama_url = os.getenv("OLLAMA_HOST", "http://localhost:11434")
 
-embeddings = OllamaEmbeddings(model="llama3")
+# GEMINI SETUP 
+gemini_key = os.getenv("GOOGLE_API_KEY")
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=gemini_key)
+embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004", google_api_key=gemini_key)
+
+# OLLAMA SETUP
+# ollama_url = os.getenv("OLLAMA_HOST", "http://localhost:11434")
+# embeddings = OllamaEmbeddings(model="llama3")
+# vector_db = Chroma(persist_directory="./story_db", embedding_function=embeddings)
+# llm = OllamaLLM(model="llama3")
+
+# Initialize Vector DB 
 vector_db = Chroma(persist_directory="./story_db", embedding_function=embeddings)
-llm = OllamaLLM(model="llama3")
 
 @app.get("/")
 def home():
@@ -23,13 +36,13 @@ def home():
 
 @app.post("/add_lore")
 async def add_lore(fact: str):   
-    """Save a character trait or plot point to the Story Bible."""
+    # """Save a character trait or plot point to the Story Bible."""
     vector_db.add_texts([fact])  
     return {"message": "Lore saved to Story Bible"}
 
 @app.post("/generate_with_context")
 async def generate_with_context(prompt: str):
-    """Retrieve relevant lore before generating the script."""
+    # """Retrieve relevant lore before generating the script."""
     # Search DB for the 2 most relevant past facts
     docs= vector_db.similarity_search(prompt, k=2)
     context = "\n".join([d.page_content for d in docs])
